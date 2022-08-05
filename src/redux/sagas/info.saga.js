00fd84acc10121848bc83b2e39dd41ba32acc670
Diego Baeza <diegob12@live.com>
2022-08-05 09:04:48 -0500

    add generator function for coordinates

--- src/redux/sagas/info.saga.js
+++ src/redux/sagas/info.saga.js
@@ -3,6 +3,14 @@ import axios from "axios";
 import useReduxStore from "../../hooks/useReduxStore";
 import { useDispatch, useSelector } from 'react-redux';
 
+function* getCoordinates(action) {
+    try {
+      const coordinates = yield axios.get(`/api/coordinates/${action.payload}`)
+      yield put({type: 'FETCH_RESTAURANTS', payload: coordinates.data})
+    } catch (error) {
+      console.log('Error in GETting Coords', error)
+    }
+}
 
 function* infoSaga() {
     yield takeLatest('FETCH_COORDINATES', getCoordinates)
