a00bd21c755778ef3430ae73e15e25d438ec3f57
Diego Baeza <diegob12@live.com>
2022-08-05 13:39:55 -0500

    create UPDATE for database

--- server/routes/photos.router.js
+++ server/routes/photos.router.js
@@ -29,4 +29,23 @@ router.get('/:photo_reference', (req, res) => {
       });
 });
 
+router.put('/', (req, res) => {
+    console.log('this is req.params', req.body);
+    console.log('this is req.params.url', req.body.url);
+    console.log('this is req.params.place_id', req.body.placeId);
+  
+    const newUrl = req.body.url;
+    const place_id = req.body.placeId;
+        
+        const queryText = `UPDATE "Downtown_Core_Old" SET "photos_url" = $1 WHERE "place_id" = $2;`;
+        pool.query(queryText, [newUrl, place_id])
+          .then((result) => {
+            res.sendStatus(200);
+          })
+          .catch((error) => {
+            console.log('Error POST /api/feedback', error);
+            res.sendStatus(500);
+          })
+});
+
 module.exports = router;
