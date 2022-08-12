1cd07001b5183e18843a135e5dc3638c27aa4d43
Diego Baeza <diegob12@live.com>
2022-08-12 00:19:23 -0500

    style homepage content

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import useReduxStore from "../../hooks/useReduxStore";
 import axios from "axios";
-import swal from 'sweetalert';
+import swal from "sweetalert";
 
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -12,33 +12,38 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
+import Input from "@mui/material/Input";
+import Button from "@mui/material/Button";
+
 function HomePage(props) {
   const dispatch = useDispatch();
   const store = useReduxStore();
 
   const [address, setAddress] = useState("");
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     dispatch({ type: "FETCH_GEOCODING", payload: address });
+    setAddress('')
+    setShow(true);
+    swal("Loading...", "", "success");
   };
 
   const addToFavorites = (event) => {
-    console.log("this is event.target", event.target.value);
+    // console.log("this is event.target", event.target.value);
 
     if (store.user.id) {
       dispatch({ type: "POST_FAVORITE", payload: event.target.value });
       swal("🫡", "Added to favorites!", "success");
     } else {
-      swal("", "Pleae login/register", "warning");
+      swal("", "Please login/register", "warning");
     }
-
   };
 
   const addToDroolList = (event) => {
-    console.log("this is event.target", event.target.value);
+    // console.log("this is event.target", event.target.value);
 
     if (store.user.id) {
       dispatch({ type: "POST_DROOL", payload: event.target.value });
@@ -48,21 +53,46 @@ function HomePage(props) {
     }
   };
 
-  return (
+  return show ? (
     <>
-      <div>
+      <div className="home-text">
         <form onSubmit={handleSubmit}>
-          <input
+          <Input
             type="text"
             placeholder="Enter Address"
             onChange={(event) => setAddress(event.target.value)}
             value={address}
+            sx={{ width: "50%", margin: "auto" }}
           />
-          <button type="submit">Submit</button>
+          <Button
+            sx={{ width: "15%", margin: "auto" }}
+            type="submit"
+            variant="outlined"
+            color="primary"
+          >
+            Search Nearby
+          </Button>
         </form>
       </div>
-      <TableContainer sx={{height: 500, width: 1000, overflow: "hidden", overflowY: "scroll"}} className = 'container' component={Paper}>
-        <Table aria-label="simple table">
+      <TableContainer
+        sx={{
+
+          height: 500,
+          width: "80%",
+          overflow: "hidden",
+          overflowY: "scroll",
+          margin: "auto",
+        }}
+        component={Paper}
+      >
+        <Table
+          stickyHeader
+          aria-label="sticky table"
+          xs={12}
+          sm={10}
+          md={8}
+          lg={8}
+        >
           <TableHead>
             <TableRow>
               <TableCell>Restaurant</TableCell>
@@ -84,10 +114,16 @@ function HomePage(props) {
                   {restaurant.rating} ⭐️ / {restaurant.user_ratings_count}
                 </TableCell>
                 <TableCell component="th" scope="row">
-                  <img style={{borderRadius: 8}} className = 'restaurant-img' src={restaurant.photos_url} alt="" />
+                  <img
+                    style={{ borderRadius: 8 }}
+                    className="restaurant-img"
+                    src={restaurant.photos_url}
+                    alt="404"
+                  />
                 </TableCell>
                 <TableCell component="th" scope="row">
-                  <button className="btn btn_sizeSm"
+                  <button
+                    className="btn btn_sizeSm"
                     onClick={(event) => addToFavorites(event)}
                     value={[
                       restaurant.name,
@@ -98,7 +134,8 @@ function HomePage(props) {
                   >
                     Add to Favorites
                   </button>
-                  <button className="btn btn_sizeSm"
+                  <button
+                    className="btn btn_sizeSm"
                     onClick={(event) => addToDroolList(event)}
                     value={[restaurant.name, restaurant.place_location]}
                   >
@@ -111,6 +148,28 @@ function HomePage(props) {
         </Table>
       </TableContainer>
     </>
+  ) : (
+    <div className="home-text">
+      <h1>Welcome to FoodieFinds!</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          placeholder="Enter Address"
+          onChange={(event) => setAddress(event.target.value)}
+          value={address}
+          sx={{ width: "50%", margin: "auto" }}
+        />
+        <Button
+          sx={{ width: "15%", margin: "auto" }}
+          type="submit"
+          variant="outlined"
+          color="primary"
+        >
+          Search Nearby
+        </Button>
+      </form>
+      <h3>Enter a valid address to begin</h3>
+    </div>
   );
 }
 
